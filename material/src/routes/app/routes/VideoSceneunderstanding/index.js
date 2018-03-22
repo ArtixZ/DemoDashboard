cc490d688fc978fc56e5e72907f6ec3a0744ab58
ArtixZ <artixzhu@gmail.com>
2018-03-21 20:29:23 -0400

    add filter

--- material/src/routes/app/routes/VideoSceneunderstanding/index.js
+++ material/src/routes/app/routes/VideoSceneunderstanding/index.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
-import c3 from 'c3'
 import QueueAnim from 'rc-queue-anim';
 import { Player, ControlBar, BigPlayButton } from 'video-react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import {GridList, GridTile} from 'material-ui/GridList';
+import TextField from 'material-ui/TextField';
+
 import vod from './cartoon.mp4';
 import "video-react/dist/video-react.css";
 import "./style.scss"
@@ -11,7 +12,13 @@ const datapoints = require("./timeStamp.json");
 
 
 export default class VideoSceneUnderstanding extends Component {
-    
+    constructor(props) {
+        super(props)
+        this.state={
+            searchVal: '',
+            datapoints: datapoints
+        }
+    }
     componentDidMount() {
         
     }
@@ -22,6 +29,22 @@ export default class VideoSceneUnderstanding extends Component {
 
     seek = (seconds) => {
         this.refs.player.seek(seconds);
+    }
+
+    onSearch = (e) => {
+        const { value } = e.target
+        this.setState
+        const filteredData = datapoints.filter((clip) => {
+            const { keywords } = clip;
+            const found = keywords.find((keyword) => {
+                return keyword.startsWith(value)
+            })
+            return !!found
+        })
+        this.setState({ 
+            searchVal: value,
+            datapoints: filteredData
+        })
     }
 
     render() {
@@ -49,44 +72,33 @@ export default class VideoSceneUnderstanding extends Component {
                                 </div>
                             </div>
                         </div>
-                        {/* <div className="col-xl-4">
-                            <div className="box box-default"> */}
-                                {/* <div className="box-body" style={{height: 485, overflow: 'scroll'}}>
-                                    {
-                                        datapoints.map((item, index) => (
-                                            <Card className="clip-item" key={index} onClick={()=>this.onClipClick(item.Timecode)}>
-                                                <CardHeader
-                                                    title={item.Timecode}
-                                                    subtitle="Subtitle"
-                                                    avatar={require(`./IndexPictures/cartoon.mp4.Scene-${index+1}-OUT.jpg`)}
-                                                />
-                                            </Card>
-                                          ))
-                                    }
-                                </div> */}
-                                <div style={styles.root}>
-                                    <GridList
-                                        cols={1}
-                                        cellHeight={150}
-                                        padding={2}
-                                        style={styles.gridList}
-                                    >
-                                    {datapoints.map((item, index) => (
-                                        <GridTile
-                                            className="clip-item"
-                                            onClick={()=>this.onClipClick(item.Timecode)}
-                                            key={index}
-                                            title={item.Timecode}
-                                            titlePosition="bottom"
-                                            titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
-                                        >
-                                            <img src={require(`./IndexPictures/cartoon.mp4.Scene-${index+1}-OUT.jpg`)} />
-                                        </GridTile>
-                                    ))}
-                                    </GridList>
-                                </div>
-                            {/* </div>
-                        </div> */}
+                        <div style={styles.root}>
+                            <GridList
+                                cols={1}
+                                cellHeight={150}
+                                padding={2}
+                                style={styles.gridList}
+                            >
+                            {this.state.datapoints.map((item, index) => (
+                                <GridTile
+                                    className="clip-item"
+                                    onClick={()=>this.onClipClick(item.Timecode)}
+                                    key={index}
+                                    title={item.Timecode}
+                                    titlePosition="bottom"
+                                    titleBackground="linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
+                                >
+                                    <img src={require(`./IndexPictures/cartoon.mp4.Scene-${index+1}-OUT.jpg`)} />
+                                </GridTile>
+                            ))}
+                            </GridList>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-xl-7" />
+                        <div className="col-xl-5">
+                            <TextField hintText="Search..." value={this.state.searchVal} onChange={this.onSearch} />
+                        </div>
                     </div>
                 </div>
                 
